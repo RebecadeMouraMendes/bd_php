7e7da054f248aa06294439920f59be38e7f2373b
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2024-05-21 12:26:49 -0300

    Add files via upload

--- Banco_de_dados/controller/pessoaController.php
+++ Banco_de_dados/controller/pessoaController.php
@@ -1,27 +1,28 @@
 <?php
 
-require_once 'C:\xampp\htdocs\Banco_de_dados\model\pessoa.php';
+require_once '../model/pessoa.php';
 
 class PessoaController{
     private $pessoa;
-
+    
     public function __construct(){
         $this->pessoa = new Pessoa();
+        $this->inserir();
     }
 
     public function inserir(){
         $this->pessoa->setNome($_POST['nome']);
-        $this->pessoa->setNome($_POST['endereco']);
-        $this->pessoa->setNome($_POST['bairro']);
-        $this->pessoa->setNome($_POST['cep']);
-        $this->pessoa->setNome($_POST['cidade']);
-        $this->pessoa->setNome($_POST['estado']);
-        $this->pessoa->setNome($_POST['telefone']);
-        $this->pessoa->setNome($_POST['celular']);
-
-        $this ->pessoa->inserir();
+        $this->pessoa->setEndereco($_POST['endereco']);
+        $this->pessoa->setBairro($_POST['bairro']);
+        $this->pessoa->setCep($_POST['cep']);
+        $this->pessoa->setCidade($_POST['cidade']);
+        $this->pessoa->setEstado($_POST['estado']);
+        $this->pessoa->setTelefone($_POST['telefone']);
+        $this->pessoa->setCelular($_POST['celular']);
         
+        $this->pessoa->inserir();
+
     }
 }
-
+new PessoaController();
 ?>
--- Banco_de_dados/model/pessoa.php
+++ Banco_de_dados/model/pessoa.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../controller/conexao.php';
 
 class Pessoa{
     private $id;
@@ -7,82 +8,77 @@ class Pessoa{
     private $bairro;
     private $cep;
     private $cidade;
+    private $estado;
     private $telefone;
     private $celular;
+    private $conexao;
 
     public function getId(){
         return $this->id;
     }
-
     public function setId($id){
         $this->id = $id;
-    }
 
+    }
     public function getNome(){
         return $this->nome;
     }
-
     public function setNome($nome){
         $this->nome = $nome;
     }
-
     public function getEndereco(){
         return $this->endereco;
     }
-
     public function setEndereco($endereco){
         $this->endereco = $endereco;
     }
-
     public function getBairro(){
         return $this->bairro;
     }
-
     public function setBairro($bairro){
         $this->bairro = $bairro;
     }
-
     public function getCep(){
         return $this->cep;
     }
-
     public function setCep($cep){
         $this->cep = $cep;
     }
-
     public function getCidade(){
         return $this->cidade;
     }
-
     public function setCidade($cidade){
         $this->cidade = $cidade;
     }
-
+    public function getEstado(){
+        return $this->estado;
+    }
+    public function setEstado($estado){
+        $this->estado = $estado;
+    }
     public function getTelefone(){
         return $this->telefone;
     }
-
     public function setTelefone($telefone){
         $this->telefone = $telefone;
     }
-
     public function getCelular(){
         return $this->celular;
     }
-
     public function setCelular($celular){
         $this->celular = $celular;
     }
-
-    public function __construct($celular){
+    
+    public function __construct(){
         $this->conexao = new Conexao();
     }
 
-    public function inserir (){
-        $sql = "INSERT INTO pessoa (`nome`,`endereco`,`bairro`,`cep`,`cidade`,`estado`,`telefone`,`celular`) VALUES (?,?,?,?,?,?,?,?)"
-        $stmt = $this->conexao->getconexao()->prepare($sql);
+    public function inserir(){
+        $sql = "INSERT INTO pessoa (`nome`, `endereco`, `bairro`, `cep`, `cidade`, `estado`, `telefone`, `celular`) VALUES (?,?,?,?,?,?,?,?)";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
         $stmt->bind_param('ssssssss', $this->nome, $this->endereco, $this->bairro, $this->cep, $this->cidade, $this->estado, $this->telefone, $this->celular);
-        return $stmt -> execute();
+        return $stmt->execute();
     }
 }
+
 ?>
